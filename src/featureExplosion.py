10f58c0a52747ab5fd87004ef5ef40120c2fadf0
PTK <pratik.irudayaraj@gmail.com>
2015-06-10 21:59:40 -0400

    Learning curves

--- src/featureExplosion.py
+++ src/featureExplosion.py
@@ -8,14 +8,83 @@ from sklearn import ensemble, preprocessing
 from sklearn import cross_validation as CV
 from sklearn.metrics import mean_squared_error
 from sklearn.utils import shuffle
+from sklearn.learning_curve import learning_curve
+import matplotlib.pyplot as plt
+
+
+# Define utility functions
+
+#1) Plot learning curves
+def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None, n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    """
+    Generate a simple plot of the test and traning learning curve.
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_learning_curve.html#example-model-selection-plot-learning-curve-py
+    Parameters
+    ----------
+    estimator : object type that implements the "fit" and "predict" methods
+        An object of that type which is cloned for each validation.
+
+    title : string
+        Title for the chart.
+
+    X : array-like, shape (n_samples, n_features)
+        Training vector, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    y : array-like, shape (n_samples) or (n_samples, n_features), optional
+        Target relative to X for classification or regression;
+        None for unsupervised learning.
+
+    ylim : tuple, shape (ymin, ymax), optional
+        Defines minimum and maximum yvalues plotted.
+
+    cv : integer, cross-validation generator, optional
+        If an integer is passed, it is the number of folds (defaults to 3).
+        Specific cross-validation objects can be passed, see
+        sklearn.cross_validation module for the list of possible objects
+
+    n_jobs : integer, optional
+        Number of jobs to run in parallel (default 1).
+    """
+    plt.figure()
+    plt.title(title)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+
+
+
+
 
 # Load dataset
 
-train_data = pd.read_csv('../input/train.csv')
+train_data = pd.read_csv('../../../west_nile/input/train.csv')
 #train_data = pd.read_csv('trainZeroFilled.csv',delimiter="|")
-test_data = pd.read_csv('../input/test.csv')
-sample_data = pd.read_csv('../input/sampleSubmission.csv')
-weather_data = pd.read_csv('../input/weather.csv')
+test_data = pd.read_csv('../../../west_nile/input/test.csv')
+sample_data = pd.read_csv('../../../west_nile/input/sampleSubmission.csv')
+weather_data = pd.read_csv('../../../west_nile/input/weather.csv')
 
 # Confirm data 
 """
@@ -169,3 +238,9 @@ sample_data.to_csv('randomForestClassifier_addedFeatures_depth30.csv', index=Fal
 #print mean_squared_error(labels_data_tail, predictions_tail)
 
 print CV.cross_val_score(clf, train_data, labels, cv=5) 
+
+
+title = "Learning Curves - Random Forest"
+plot_learning_curve(clf, title, train_data, labels, (0.7, 1.01), cv=None, n_jobs=4)
+
+plt.show()
